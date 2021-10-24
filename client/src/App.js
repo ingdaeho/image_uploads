af0d558cbb7d94afbf7a30d3125ac49af1065299
daeho <dlaeogh8572@gmail.com>
2021-10-24 17:20:19 +0900

    add: client imageList

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import UploadForm from "./components/UploadForm";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ImageList from "./components/ImageList";
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/images")
+      .then((res) => setImages(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div>
+    <div style={{ maxWidth: 600, margin: "auto" }}>
       <ToastContainer />
       <h2>사진첩</h2>
-      <UploadForm />
+      <UploadForm images={images} setImages={setImages} />
+      <ImageList images={images} />
     </div>
   );
 }
---
+++ client/src/components/ImageList.js
@@ -0,0 +1,17 @@
+const ImageList = ({ images }) => {
+  const imgList = images.map((image) => (
+    <img
+      key={image.key}
+      style={{ width: "100%" }}
+      src={`http://localhost:5000/uploads/${image.key}`}
+      alt="images"
+    />
+  ));
+  return (
+    <div>
+      <h3>Image List</h3>
+      {imgList}
+    </div>
+  );
+};
+export default ImageList;
